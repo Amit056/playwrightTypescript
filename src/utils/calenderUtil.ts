4e5d87f14b8eca8b8a1eef5f29ad53b6054f3827
Amit Kumar Ray <120637152+Amit056@users.noreply.github.com>
2025-12-13 08:09:43 +0530

    Fix duplicate click on date selection

--- src/utils/calenderUtil.ts
+++ src/utils/calenderUtil.ts
@@ -56,4 +56,5 @@ export async function selectDate(
 
   /** Step 3: Select the date */
   await page.locator(`li[id$="_${idSuffix}"]`).click();
+  await page.locator(`li[id$="_${idSuffix}"]`).click();
 }
